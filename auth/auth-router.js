26e2f510424c14eb97a90770788930f9b7709659
Brianna Workman <brianna.workman@hotmail.com>
2019-12-16 18:17:55 -0600

    Complete MVP

---
+++ auth/auth-router.js
@@ -0,0 +1,41 @@
+const bcrypt = require("bcryptjs");
+const router = require("express").Router();
+const Users = require("../users/users-model.js");
+
+router.post("/register", (req, res) => {
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: "Error registering the user" });
+    });
+});
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res
+          .status(200)
+          .json({ message: `Login Successful. Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: "You shall not pass!" });
+    });
+});
+
+module.exports = router;
